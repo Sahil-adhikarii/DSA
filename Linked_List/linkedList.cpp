bfdca59150516daca588fe7e6ad95b038c16bdef
Sahil-adhikarii <adhikari321sahil@gmail.com>
2025-11-26 07:47:46 +0545

    Updated

--- Linked_List/linkedList.cpp
+++ Linked_List/linkedList.cpp
@@ -43,8 +43,28 @@ class linked_list{
     }
 
 
-    
+    void insert_at_position( int data,int position){
+    node* n=new node;
+    n->data=data;
+    n->next=NULL;
 
+
+    node* temp=head;
+   int i=0;
+   while (i < position-1)
+   {
+        if(temp==NULL){
+            cout<<"Position out of bounds"<<endl;
+            delete n;
+            return;
+        }
+        temp=temp->next;
+        i++;
+    }
+
+    n->next=temp->next;
+    temp->next=n;
+}
 
 
        void delete_at_head(){
@@ -63,12 +83,6 @@ void delete_at_tail(){
             cout << "List is empty\n";
             return;
         }
-
-        // if(head->next == NULL){
-        //     delete head;
-        //     head = NULL;
-        //     return;
-        // }
 
         node *temp = head;
         while(temp->next->next != NULL){
@@ -100,16 +114,19 @@ int main(){
     l.Insert_at_Tail(13);
     l.Insert_at_Tail(14);
     
+    l.insert_at_position(15, 2);
     
 
-    l.delete_at_head();
-    l.delete_at_head();
-    l.delete_at_head();
-    l.delete_at_head();
+    // l.delete_at_head();
+    // l.delete_at_head();
+    // l.delete_at_head();
+    // l.delete_at_head();
+    // l.delete_at_head();
 
-        
-
-    l.delete_at_tail();    
+    // l.delete_at_tail();    
+    // l.delete_at_tail();    
+    // l.delete_at_tail();    
+    // l.delete_at_tail();    
 
     l.display();
 }
