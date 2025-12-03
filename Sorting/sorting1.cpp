8cc68a86b6bd7289565d40416f0624f835fa96f0
Sahil-adhikarii <adhikari321sahil@gmail.com>
2025-12-03 10:53:13 +0545

    Push Pop

---
+++ Sorting/sorting1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int a[]={2,342,13,45,13,12,34};
+int size=sizeof(a)/sizeof(a[0]);
+
+int main(){
+
+    for(int i=0;i<size;i++){
+
+        for(int j=i+1;j<size;j++){
+            
+        }
+    }
+
+    return 0;
+}
