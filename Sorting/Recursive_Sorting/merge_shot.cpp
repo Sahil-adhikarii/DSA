667ff021ebf7dc5bda59d19c8b2d6bad6bf4d635
Sahil-adhikarii <adhikari321sahil@gmail.com>
2025-12-07 23:45:08 +0545

    merge sorting

---
+++ Sorting/Recursive_Sorting/merge_shot.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+int x[]={1,2,3,4,5,6,7};
+int y[]={8,9,10,11,12,13};
+int i=0,j=0,k=0;
+
+    int sizeX = sizeof(x)/sizeof(x[0]);
+    int sizeY = sizeof(y)/sizeof(y[0]);
+    int totalSize = sizeX+sizeY;
+
+int main(){
+    // int a[3] = {4,5,6};
+   int res[totalSize];
+
+
+    while(i<sizeX && j<sizeY){
+        if(x[i]<y[j]){
+            res[k] = x[i];
+            i++;
+        }
+        else{
+            res[k] = y[j];
+            j++;
+        }
+        k++;
+    }
+    
+    while(i<sizeX){
+        res[k] = x[i];
+        i++;
+        k++;
+        }
+    while(j<sizeY){
+      res[k] = y[j];
+      j++;
+      k++;
+    }
+
+        for(i=0;i<totalSize;i++){
+            cout<<res[i]<<endl;
+        }
+
+    return 0;
+}
