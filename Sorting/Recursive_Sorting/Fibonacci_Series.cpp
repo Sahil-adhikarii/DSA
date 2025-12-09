cbad556cd9ee63dd78ab2cfcb348dc496cd4ccc0
Sahil-adhikarii <adhikari321sahil@gmail.com>
2025-12-09 09:49:51 +0545

    Fibonacci_Series

---
+++ Sorting/Recursive_Sorting/Fibonacci_Series.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+#include <iostream>
+using namespace std;
+
+int fib(int n) {
+    if (n == 0)  return 0; 
+        
+    else if (n==1) return 1;
+
+    else return (fib(n-1) + fib(n-2)); 
+}
+
+int main() {
+    int n=90;
+
+        // cout<<fib(6)<<endl;
+
+    for(int i=0;i<n;i++) 
+    {   
+        cout<<fib(i)<<"->";
+    }
+
+    return 0;
+}
+
